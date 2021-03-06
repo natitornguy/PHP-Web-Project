c348a68c238855a3f29cd246216d0ae684c947bd
haki555 <71861752+haki555@users.noreply.github.com>
2021-03-06 15:52:41 +0700

    hello

---
+++ views/register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>สมัครสมาชิก</title>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+		<form>
+			<h2>Register</h2>
+			<label>User Name :</label>
+			<input type="text" name="unname" placeholder="User Name"><br>
+<br>
+
+			<label>Password : </label>
+			<input type="Password" name="Password" placeholder="Password"><br>
+<br>
+
+			<button type="submit">Create</button>
+<br>
+<br>
+			<a href="login.php" class="ca">Have a Account ?</a>
+		</form>
+
+</body>
+</html>
