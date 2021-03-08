de64df59744c181379925fc279e0e7cb0b6a22c6
natitornguy <ka.natitorn_st@tni.ac.th>
2021-03-08 12:19:36 +0700

    Last update

--- application/views/leave/leaveform.php
+++ application/views/leave/leaveform.php
@@ -1,72 +1,62 @@
-<div class="wrapper ">    
-    <div class="main-panel">        
-        <div class="content">
-            <div class="container-fluid">
-                <!-- your content here -->
-                <div class="container">
-                    <!-- <div class="form-group"> -->
-                    <h2>แบบฟอร์มขอลา</h2>
-                    <p class="subhead">กรุณากรอกข้อมูลให้ครบถ้วน</p>
-                    <!-- </div> -->
-                    <div class="container">
-                        <form method="post" action="<?= base_url("LeaveRequest/addleavesave/") ?> ">
-                            <div class="row">
+<div class="wrapper ">
+    <div class="main-panel">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header card-header-primary">
+                    <h4 class="card-title">แบบฟอร์มขอลา</h4>
+                    <p class="card-category">กรุณากรอกข้อมูลให้ครบถ้วน</p>
+                </div>
+                <!-- end of header -->
 
-                                <div class="col-6">
-                                    <p>หยุดวันที่</p>
-                                </div>
-                                <div class="col-6">
-                                    <p>ถึงวันที่</p>
-                                </div>
-                            </div>
+                <!-- content -->
+                <div class="card-body">
+                    <form method="post" action="<?= base_url("LeaveRequest/addleavesave/") ?>">
+                        <div class="row">
                             <?php
                             $startdate = is_object($info) ? $info->LEAVE_FROM : "";
                             $enddate = is_object($info) ? $info->LEAVE_TO : "";
                             ?>
-                            <div class="row">
-                                <div class="col-6">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="bmd-label-floating mr-2">หยุดวันที่</label>
                                     <input type="date" id="startDate" name="startDate" width="276" required value="<?= $startdate ?>" />
                                 </div>
-                                <div class="col-6">
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label class="bmd-label-floating mr-2">ถึงวันที่</label>
                                     <input type="date" id="endDate" name="endDate" width="276" required value="<?= $enddate ?>" />
                                 </div>
                             </div>
-
-                            <div class="row">
-                                <div class="col">
-                                    <p>เหตุผลขอลาหยุด</p>
-                                </div>
-                            </div>
-                            <?php
-                            $reason = is_object($info) ? $info->LEAVE_REASON : "";
-                            ?>
-                            <div class="row">
-                                <div class="col-6">
+                        </div>
+                        <?php
+                        $reason = is_object($info) ? $info->LEAVE_REASON : "";
+                        ?>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label class="bmd-label-floating">เหตุผลขอลาหยุด</label>
                                     <textarea class="form-control" id="reason" name="reason" rows="4" cols="50" required><?= $reason ?></textarea>
                                 </div>
                             </div>
-                            <input type="hidden" id="method" name="method" value="<?= $method ?>">
-                            <input type="hidden" id="leaveID" name="leaveID" value="<?= is_object($info) ? $info->LEAVE_ID : "" ?>">
-                            <div class="row">
-                                <div class="col-12">
-                                    <button id="btnSubmit" name="btnSubmit" type="submit" class="btn btn-primary">
-                                        ยืนยัน
-                                    </button>
-                                    <button type="reset" id="btn_reset" name="btn_reset" class="btn btn-secondary">
-                                        ล้าง
-                                    </button>
-                                    <a id="btn_cancel" name="btn_cancel" class="btn btn-danger" href="<?= base_url("LeaveRequest/index") ?>">
-                                        ยกเลิก
-                                    </a>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+                        </div>
+                        <input type="hidden" id="method" name="method" value="<?= $method ?>">
+                        <input type="hidden" id="leaveID" name="leaveID" value="<?= is_object($info) ? $info->LEAVE_ID : "" ?>">
+                        <div class="row mt-2">
+                            <div class="col-md-3"></div>
+                            <button type="submit" class="btn btn-success pull-right">ยืนยัน</button>
+                            <button type="reset" class="btn btn-default pull-right ml-2">เคลียร์</button>
+                            <a id="btn_cancel" name="btn_cancel" href="<?= base_url("LeaveRequest/index") ?>" class="btn btn-danger pull-right ml-2">ยกเลิก</a>
+                            <div class="clearfix"></div>
+                        </div>
+                    </form>
                 </div>
+                <!-- end of content -->
             </div>
-
+            <!-- end of body -->
         </div>
     </div>
+</div>
 </div>
 <script>
     document.getElementById("startDate").setAttribute("min", getDateString(new Date()));
